1312f03c17cb1e73bbfe7a7adc72ba15786d57b5
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2025-03-13 16:57:32 -0700

    implement ASCII tree drawing function

--- file2.py
+++ file2.py
@@ -1,6 +1,17 @@
-def greet_2(name):
-    return f"Hello, {name}!"
+def draw_ascii_tree(height):
+    if height < 1:
+        return "Height must be at least 1"
+    
+    tree = ""
+    for i in range(height):
+        spaces = ' ' * (height - i - 1)
+        stars = '*' * (2 * i + 1)
+        tree += spaces + stars + '\n'
+    
+    trunk = ' ' * (height - 1) + '*\n'
+    tree += trunk
+    return tree
 
 if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print(greet_2(name))
+    height = 5
+    print(draw_ascii_tree(height))
